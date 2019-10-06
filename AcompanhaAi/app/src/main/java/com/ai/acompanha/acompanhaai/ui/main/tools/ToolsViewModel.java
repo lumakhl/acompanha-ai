344717abcd9de30a21b254a60c24842c373f24a8
lumakhl <kuhl.luma@gmail.com>
2019-10-06 19:02:52 -0300

    adicionando menu lateral

---
+++ AcompanhaAi/app/src/main/java/com/ai/acompanha/acompanhaai/ui/main/tools/ToolsViewModel.java
@@ -0,0 +1,19 @@
+package com.ai.acompanha.acompanhaai.ui.main.tools;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ToolsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ToolsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is tools fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
