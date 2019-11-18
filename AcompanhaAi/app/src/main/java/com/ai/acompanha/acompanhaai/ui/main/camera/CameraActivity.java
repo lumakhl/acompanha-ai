3d407b9bc025b41cee0fe260319061606c26601b
AlexSerodio <alequis01@gmail.com>
2019-11-17 21:38:41 -0300

    adicionado CameraActivity e CameraPreview

---
+++ AcompanhaAi/app/src/main/java/com/ai/acompanha/acompanhaai/ui/main/camera/CameraActivity.java
@@ -0,0 +1,42 @@
+package com.ai.acompanha.acompanhaai.ui.main.camera;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.ai.acompanha.acompanhaai.R;
+
+public class CameraActivity extends Activity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    private static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+}
+
