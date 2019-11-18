15d1acd6c7e6cb8fc36e4964699fdcc8b61d28ca
AlexSerodio <alequis01@gmail.com>
2019-11-18 00:12:00 -0300

    adicionado evento para capturar a imagem ao clicar no botão da camera

--- AcompanhaAi/app/src/main/java/com/ai/acompanha/acompanhaai/ui/main/camera/CameraActivity.java
+++ AcompanhaAi/app/src/main/java/com/ai/acompanha/acompanhaai/ui/main/camera/CameraActivity.java
@@ -1,16 +1,37 @@
 package com.ai.acompanha.acompanhaai.ui.main.camera;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.ai.acompanha.acompanhaai.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Arrays;
 
 public class CameraActivity extends Activity {
 
     private Camera mCamera;
     private CameraPreview mPreview;
+
+    private PictureCallback mPicture = new PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+            if(bitmap != null)
+                Log.d("Info", "Imagem convertida para bitmap com sucesso.");
+            else
+                Log.d("Info", "Ocorreu um erro ao tentar converter a imagem para bitmap.");
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +43,23 @@ public class CameraActivity extends Activity {
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+
+        FloatingActionButton captureButton = findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mCamera.takePicture(null, null, mPicture);
+                }
+            }
+        );
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
     }
 
     private static Camera getCameraInstance(){
@@ -31,6 +69,13 @@ public class CameraActivity extends Activity {
         } catch (Exception e){ }
 
         return camera;
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();
+            mCamera = null;
+        }
     }
 
 }
